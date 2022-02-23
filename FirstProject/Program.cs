d5d3a14b78e3da254d3114ba4fd36962bf57db7a
vlad <vlad.lychagin@gmail.com>
2022-02-23 17:44:39 +0300

    Начало проекта

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\n\nПривет, мир!\nСегодня {DateTime.Now:G}");
+        }
+    }
+}
