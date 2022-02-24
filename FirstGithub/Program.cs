554da126f3c41b77a467ac807117a75cc560a6fe
vlad <vlad.lychagin@gmail.com>
2022-02-24 17:01:03 +0300

    Project renamed

--- FirstGithub/Program.cs
+++ FirstGithub/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstProject
+namespace FirstGithub
 {
     internal class Program
     {
@@ -13,6 +13,8 @@ namespace FirstProject
             Console.WriteLine($"\n\nПривет, мир!\nСегодня {DateTime.Now:G}");
             double r = 100d;
             Console.WriteLine($"r = {r:f3}");
+			r = r/d2;
+			Console.WriteLine($"r = {r:f3}");
         }
     }
 }
--- FirstGithub/Properties/AssemblyInfo.cs
+++ FirstGithub/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("FirstProject")]
+[assembly: AssemblyTitle("FirstGithub")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FirstProject")]
+[assembly: AssemblyProduct("FirstGithub")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
