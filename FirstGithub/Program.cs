174ed926d10a18c760d943f6cda9f99177d0f472
vlad <vlad.lychagin@gmail.com>
2022-03-02 15:45:54 +0300

    продолжаем редактирование проекта

--- FirstGithub/Program.cs
+++ FirstGithub/Program.cs
@@ -11,13 +11,17 @@ namespace FirstGithub
         static void Main(string[] args)
         {
             // ветвь master
-            Console.Title = "Пример проекта";
+            Console.Title = "Пример проекта c геометрическими построениями";
             
             Console.WriteLine($"\n\nПривет, мир!\nСегодня {DateTime.Now:G}");
             double r = 100d;
             double area = Math.PI * r*r;
 
 			Console.WriteLine($"r = {r:f3} ед., area = {area:f3} кв.ед.");
+
+            // расчет объема сферы
+            double volume = 4 * Math.PI * r * r * r / 3;
+            Console.WriteLine($"объем сферы радиуса {r:f3} равен {volume:f3}");
         }
     }
 }
